e904961c2dcc24a8f2282ac90516230ef5100ef6
MRHackerx00 <169080023+MRHackerx00@users.noreply.github.com>
2024-06-05 11:56:45 +0530

    Add files via upload

---
+++ mainbot.py
@@ -0,0 +1,109 @@
+import telebot
+from datetime import datetime
+import random
+from googlesearch import search
+import requests              
+import PyPDF2
+from pypdf import PdfReader
+from io import BytesIO
+import os
+
+
+bot = telebot.TeleBot("6177942697:AAG8Xg-tk8jtDMfCgViJCgxOzQk5adK3Mvk")
+channel_id = '@hackerpdfbotlog'
+c = datetime.now()
+time = c.strftime('%H:%M:%S')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+
+  bot.send_message(message.chat.id, f"""
+*hi, @{message.from_user.username} 
+this bot send learning PDF*
+
+use:
+```use
+search: <you topis search > 
+```
+*🗒️🚨 note: use small charter use search:*
+
+*owner : @MR_Hacker_000*""", parse_mode="Markdown")
+  id = message.from_user.id                                             
+  first_name = message.from_user.first_name
+  last_name = message.from_user.last_name                               
+  username = message.from_user.username
+  bot.send_message(channel_id, f"""    
+*@{username}
+This started bot
+
+id - {id}
+name - {first_name}
+last name - {last_name}
+username - @{username}
+
+🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕
+*
+
+""", parse_mode="Markdown")
+
+@bot.message_handler(func=lambda message: True)
+def handle_user_input(message):
+    if message.text.startswith('search: '):
+        global sech, j
+        sech = message.text.split('search: ', 1)[1].strip()
+        for j in search(f"{sech} filetype; pdf", num=3, stop=3, pause=2):
+
+
+          global username, last_name, first_name, id
+          id = message.from_user.id
+          first_name = message.from_user.first_name
+          last_name = message.from_user.last_name
+          username = message.from_user.username
+          response = requests.get(j)
+          pdf_content = BytesIO(response.content)
+          reader = PdfReader(pdf_content)
+          file =f"pdfcreation date :_   {reader.metadata.creation_date} "
+
+          try:
+            bot.send_document(message.chat.id, j, caption=file) 
+            bot.send_message(channel_id, f"""
+*doc log
+id - {id}
+name - {first_name}
+last name - {last_name}
+username - @{username}
+search - {sech}
+link - {j}
+file info: {file}
+today data -  {c}
+*
+
+""", parse_mode="Markdown")
+          except:
+               global text
+               text = f"* this is pdf download  link :-*  [link]({j})"
+               bot.send_message(message.chat.id, text, parse_mode="Markdown")
+               # Send a message to the channel_id
+
+        bot.send_message(channel_id, f"""
+*text log
+id - {id}
+name - {first_name} 
+last name - {last_name}
+username - @{username}
+search - {sech}
+link - {j}
+today -  {c}*
+
+""", parse_mode="Markdown")
+
+#          finally:
+              
+
+    else:
+        bot.reply_to(message, "I'm sorry, I don't und	erstand that command.")
+
+
+
+
+bot.infinity_polling()
